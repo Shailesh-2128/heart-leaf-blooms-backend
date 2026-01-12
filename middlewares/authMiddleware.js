acb6a9e31f0aecb842bb56556404dde115437afd
Shailesh-2128 <shaileshjiwana15219@gmail.com>
2026-01-12 12:57:02 +0530

    auth error fix

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,7 +2,9 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
     const token = req.cookies.token || req.cookies.user_token || req.cookies.vendor_token;
-
+    console.log(token);
+    console.log(req.cookies);
+    console.log(req);
     if (!token) {
         return res.status(401).json({ error: "Access denied. Not authenticated." });
     }
