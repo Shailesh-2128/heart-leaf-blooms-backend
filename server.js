391be124b687e2061f9598bb89a6f302dd17b2e0
Shailesh-2128 <shaileshjiwana15219@gmail.com>
2026-01-16 17:44:09 +0530

    server.js updated

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const cartRoutes = require("./routes/cartRoutes");
 const wishlistRoutes = require("./routes/wishlistRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
 const adminProductRoutes = require("./routes/adminProductRoutes");
+const adminBannerRoutes = require("./routes/adminBannerRoutes");
 const reviewRoutes = require("./routes/reviewRoutes");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./config/swagger");
@@ -21,8 +22,8 @@ const cors = require("cors");
 
 const cookieParser = require("cookie-parser");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(cors({
@@ -37,10 +38,9 @@ app.use(compression());
 
 // Request Logger
 const requestLogger = require("./middlewares/requestLogger");
-app.use(requestLogger);
+// app.use(requestLogger);
 
 app.get("/", (req, res) => {
-
     res.send("blooms server is running");
 });
 
@@ -55,6 +55,13 @@ app.use("/cart", cartRoutes);
 app.use("/wishlist", wishlistRoutes);
 app.use("/payment", paymentRoutes);
 app.use("/review", reviewRoutes);
+app.use("/banner", adminBannerRoutes);
+
+const adminInvoiceRoutes = require("./routes/adminInvoiceRoutes");
+const invoiceRoutes = require("./routes/invoiceRoutes");
+
+app.use("/admin", adminInvoiceRoutes); // /admin/invoice-settings
+app.use("/invoice", invoiceRoutes); // /invoice/...
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
